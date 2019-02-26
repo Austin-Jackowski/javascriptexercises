005956caff9290d1e233beacea840c8ad53055d8
Austin Jackowski <ajackowski@gmail.com>
2019-02-26 18:08:15 -0500

    Add preliminary functionality to snakeCase()

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,7 @@
-const snakeCase = function() {
-
+const snakeCase = function (string) {
+  let filter = /[@.,/#!$%^&*;:{}=/-_`~()]/g
+  let filteredString = string.toLowerCase().replace(filter, '_')
+  return filteredString
 }
 
 module.exports = snakeCase

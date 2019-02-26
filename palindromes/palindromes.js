0dc22af74d87d9af3fafba6c50469d824bb4ed2c
Austin Jackowski <ajackowski@gmail.com>
2019-02-26 17:37:47 -0500

    Add palindrome() function, fix jasmine tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function (string) {
+  let filter = /[@.,\/#!$%\^&\*;:{}=/-_`~()\s]/g
+  let filteredString = string.toLowerCase().replace(filter, '')
+  let reverseString = filteredString.split('').reverse().join('')
 
+  if (filteredString === reverseString) {
+    return true
+  }
+  return false
 }
 
 module.exports = palindromes
